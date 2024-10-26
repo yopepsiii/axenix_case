16e2ed911deea6d132d37747a2b97b9723bb5675
BazookaOrangutan <kolyache280@gmail.com>
2024-10-26 12:08:33 +0300

    создан header

---
+++ frontend/axenix/src/components/logo/Logo.jsx
@@ -0,0 +1,11 @@
+import './Logo.css'
+
+export default function Logo(){
+    return(
+        <div className="logo">
+            <img src="/img/logo.png" alt="logo.png" />
+            <div className='ax'>ax</div>
+            <div className="tripsText">Поездки</div>
+        </div>
+    )
+}
---
+++ frontend/axenix/src/components/search/Search.jsx
@@ -0,0 +1,12 @@
+import './Search.css'
+
+export default function Search(){
+    return(
+        <form action="">
+            <input type="text" />
+            <button><img src="/img/Process.png" alt="" /></button>
+            <input type="text" />
+        </form>
+    )
+
+}
