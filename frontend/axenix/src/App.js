f19d3cc398fba74cc5044bd77f35c82ed345b3a8
BazookaOrangutan <kolyache280@gmail.com>
2024-10-26 20:18:11 +0300

    дата, тип вагона сделаны

--- frontend/axenix/src/App.js
+++ frontend/axenix/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/header/Header';
+import ParametrsPage from './pages/ParametrsPage/ParametrsPage';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ParametrsPage/>
     </div>
   );
 }
--- frontend/axenix/src/components/search/Search.jsx
+++ frontend/axenix/src/components/search/Search.jsx
@@ -2,7 +2,7 @@ import './Search.css'
 
 export default function Search(){
     return(
-        <form action="">
+        <form action="" className='search'>
             <input type="text" />
             <button><img src="/img/Process.png" alt="" /></button>
             <input type="text" />
