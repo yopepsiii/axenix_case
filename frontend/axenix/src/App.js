c3c10a4d73916e9b699afd729fc1c3039f1be611
Aizenirus <arseniofed00@gmail.com>
2024-10-26 12:52:30 +0300

    сделана основная страница

--- frontend/axenix/src/App.js
+++ frontend/axenix/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Panel from './components/panel/Panel';
 
 function App() {
   return (
     <div className="App">
-      гойда
+      
     </div>
   );
 }
 
-export default App;
+export default App; 
---
+++ frontend/axenix/src/components/panel/Panel.jsx
@@ -0,0 +1,37 @@
+import "./Panel.css"
+function Panel() {
+    return (
+        <body>
+            <div className="panel">
+                <div className="column">
+                <h1>Дайте деняг</h1>
+                <p>Думайте текст сами, бла бла бла бла</p>
+                <p>бла бла бла бла бла бла бла</p>
+                <ul>
+                    <li>
+                        <div className="galka">
+                            <img src="img/Done.svg" alt="" />
+                        </div>
+                        Можем и то</li>
+                        <li>
+                        <div className="galka">
+                            <img src="img/Done.svg" alt="" />
+                        </div>
+                        И то</li>
+                        <li>
+                        <div className="galka">
+                            <img src="img/Done.svg" alt="" />
+                        </div>
+                        И то</li>
+                </ul>
+                <input type="submit" value ="Найти" />
+                </div>
+                <img src="img/Train.svg" alt="" />
+
+            </div>
+        </body>
+
+    )
+}
+
+export default Panel
